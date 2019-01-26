458ae07c83e4c83dc9af4b276901b56280883776
Don <don.f.stegall@gmail.com>
2019-01-26 00:06:18 -0500

    edit #0001 added basic hello-goodbye functionality

--- App.js
+++ App.js
@@ -1,21 +1,90 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React  from 'react';
+import { StyleSheet, View, Text, TextInput, Button, Alert } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleClickHello = () => {
+    this.setState({ value: 'Hello' });
+  }
+
+  handleClickGoodbye = () => {
+    this.setState({value: 'Goodbye'});
+  }
+
+  handleClickUpperCase = () => {
+    this.setState({value: this.state.value.toUpperCase()});
+  }
+
+  handleClickLowerCase = () => {
+    this.setState({value: this.state.value.toLowerCase()});
+  }
+
+  handleClickMessage = () => {
+    Alert.alert("Message", this.state.value);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+
+        <Text style={styles.title}>hello-goodbye-react-native-v1</Text>
+
+        <TextInput
+          style={{width: 300, height: 40, borderColor: 'gray', borderWidth: 1, padding: 10}}
+          onChangeText={(value) => this.setState({value})}
+          value={this.state.value}
+        />
+
+        <Button
+          title="Hello"
+          onPress={this.handleClickHello} />
+
+        <Button
+          title="Goodbye"
+          onPress={this.handleClickGoodbye} />
+
+        <Button
+          title="Upper Case"
+          onPress={this.handleClickUpperCase}
+          disabled={!this.state.value} />
+
+        <Button
+          title="Lower Case"
+          onPress={this.handleClickLowerCase}
+          disabled={!this.state.value} />
+
+        <Button
+          title="Show Input Message"
+          onPress={this.handleClickMessage}
+          disabled={!this.state.value} />
+
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    top: 60
   },
+
+  title: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 20,
+    margin: 10,
+  },
+ 
 });
